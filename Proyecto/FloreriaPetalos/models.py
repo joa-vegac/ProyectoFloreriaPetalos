179d1ab29790aa51e72f68586ab6246c8c39ca05
Joa-vegac <Joa.vegac@alumnos.duoc.cl>
2019-11-24 02:51:22 -0300

    Registro de productos funcionando?

--- Proyecto/FloreriaPetalos/models.py
+++ Proyecto/FloreriaPetalos/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
-class Producto (models.Model):
-    nombre = models.CharField(max_length=50, primary_key=True)
-    valor = models.IntegerField()
-    desc = models.TextField(max_length=400)
-    image = models.ImageField(upload_to='producto',null=True)
-    stock = models.IntegerField()
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100, primary_key=True)
+    valor = models.IntegerField(default=1000)
+    descripcion = models.TextField()
+    stock = models.IntegerField(default=1)
+    imagen= models.ImageField(upload_to='productos', null=True)
 
     def __str__(self):
         return self.nombre
-
--- Proyecto/FloreriaPetalos/views.py
+++ Proyecto/FloreriaPetalos/views.py
@@ -2,36 +2,38 @@ from django.shortcuts import render
 from .models import Producto
 
 # Create your views here.
-def registro(request):
-     return render(request, 'core/registro.html')
-
-def login(request):
-    return render(request, 'core/login.html')
 
 def index(request):
     return render(request, 'core/index.html')
 
+def login(request):
+    return render(request, 'core/login.html')
+
+def registro(request):
+    return render(request, 'core/registro.html')
+
 def catalogo(request):
-    return render(request, 'core/catalogo.html')
+    productos=Producto.objects.all()
+    return render(request, 'core/catalogo.html',{'productos': productos})
   
 def registro_producto(request):
     if request.POST:
-        accion=request.POST.get("RegProduct")
-        if accion=='aceptar':            
-            nombre=request.POST.get("Name")
-            valor=request.POST.get("Value")
-            desc=request.POST.get("Desc")
-            image=request.FILES.get("Image")
-            stock=request.POST.get("Stock")
+        accion=request.POST.get("accion")
+        if accion=='registrar':
+            prdctnombre = request.POST.get("name")
+            prdctvalor = request.POST.get("value")
+            prdctdescripcion = request.POST.get("desc")
+            prdctstock = request.POST.get("stock")
+            prdctimagen= request.FILES.get("image")
 
-            product=Producto(
-                nombre=nombre,
-                valor=valor,
-                desc=desc,
-                image=image,
-                stock=stock,
-            )
-            product.save()
+        product=Producto(
+            nombre= prdctnombre,
+            valor= prdctvalor,
+            descripcion= prdctdescripcion,
+            stock= prdctstock,
+            imagen= prdctimagen
+        )
+        product.save
     return render(request,'core/registro_producto.html')
     
 
