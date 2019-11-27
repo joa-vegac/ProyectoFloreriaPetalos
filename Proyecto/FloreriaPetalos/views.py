5df2737bf12e28e7af89cd8278553952a66d789f
Joa-vegac <Joa.vegac@alumnos.duoc.cl>
2019-11-26 23:38:11 -0300

    Agregando producto

--- Proyecto/FloreriaPetalos/views.py
+++ Proyecto/FloreriaPetalos/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
 from .models import Producto
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-
-def index(request):
-    return render(request, 'core/index.html')
 
 def login(request):
     return render(request, 'core/login.html')
-
+    
 def registro(request):
     return render(request, 'core/registro.html')
 
+@login_required(login_url='/login/')
+def index(request):
+    return render(request, 'core/index.html')
+
+@login_required(login_url='/login/')
 def catalogo(request):
     productos=Producto.objects.all()
     return render(request, 'core/catalogo.html',{'productos': productos})
-  
+
+@login_required(login_url='/login/')
 def registro_producto(request):
     if request.POST:
         accion=request.POST.get("accion")
@@ -33,7 +36,7 @@ def registro_producto(request):
             stock= prdctstock,
             imagen= prdctimagen
         )
-        product.save
+        product.save()
+        return render(request, 'core/registro_producto.html', {'msg':'Producto registrado'})
     return render(request,'core/registro_producto.html')
-    
 
--- Proyecto/Proyecto/urls.py
+++ Proyecto/Proyecto/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('FloreriaPetalos.urls')),
+    path('', include("django.contrib.auth.urls")),
 ]
 
 if settings.DEBUG:
